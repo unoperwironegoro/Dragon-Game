1463da634572b7b9dcb1302f5a8fb926de97c150
Perwironegoro <hp1515@ic.ac.uk>
2018-04-19 22:17:42 +0100

    Extracted scene setup.

---
+++ Assets/Scripts/Interscene/SceneSetup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSetup : MonoBehaviour {
+    [SerializeField]
+    private string baseScene;
+    [SerializeField]
+    private string[] extraScenes;
+
+	private void Start () {
+        if(baseScene != null) {
+            SceneSwitcher.InstantSwitch("ArenaMenu", null, LoadSceneMode.Single);
+        }
+
+        foreach(string s in extraScenes) {
+            SceneSwitcher.InstantSwitch(s, null, LoadSceneMode.Additive);
+        }
+    }
+}
